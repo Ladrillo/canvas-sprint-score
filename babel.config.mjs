cd8f549c0faffde72c453d954bd3a26c3a087d79
Ladrillo <gabriel@cabrejas.net>
2021-11-27 19:15:20 +0100

    back to commonjs

--- babel.config.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-  plugins: [],
-  presets: [
-    ['@babel/preset-env', { targets: { node: 6 } }]
-  ],
-}
---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const fs = require('fs/promises')
+const path = require('path')
+
+const fileName = process.argv[2]
+
+if (!fileName) {
+  process.stderr.write('\nYou need to provide a .csv file to convert\n\n')
+  process.exit(1)
+}
+
+const minusExtension = fileName.split(/\.csv$/i)[0]
+
+const csvPath = path.resolve(__dirname, fileName)
+const convertedCsvPath = path.resolve(__dirname, `${minusExtension}-converted.csv`)
+
+fs.readFile(csvPath)
+  .then(data => {
+    const lines = data
+      .toString()
+      .trim()
+      .split('\n')
+      .slice(2)
+      .filter(line => line.slice(0, 8) !== '"Student')
+
+    const shorterLines = []
+    lines.forEach(line => {
+      const split = line.split(',')
+      const result = `${split[0]}, ${split[1]}, ${split[split.length - 1]}`
+      shorterLines.push(result)
+    })
+    return fs.writeFile(convertedCsvPath, shorterLines.join('\n'))
+  })
+  .then(() => {
+    return fs.readFile(convertedCsvPath)
+  })
+  .then((data) => {
+    process.stdout.write(data.toString())
+  })
